ed0416560388a7b05cf03d0a62f81768d3f7b7ba
Denisse Landau <denisselandau97@gmail.com>
2022-08-02 13:47:52 -0700

    upd

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(__attribute__((unused))int argc ,__attribute__((unused))char **argv)
 
 	while (1)
 	{
-		printf("#cisfun$ ");
+		/* printf("#cisfun$ "); */
         	character = getline(&buffer, &bufsize, stdin);
 		if (character == -1)
 		{
