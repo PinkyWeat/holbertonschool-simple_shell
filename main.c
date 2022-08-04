7cdf83089399deb3d725005fd50b9f2a1dd173f4
Damian Caetano <5100@holbertonschool.com>
2022-08-04 11:56:39 -0700

    fix env

--- main.c
+++ main.c
@@ -4,28 +4,25 @@
  */
 int main(__attribute__((unused))int argc ,__attribute__((unused))char **argv)
 {
-        size_t bufsize = 0, h = 0, r;
-        char *buffer = NULL, *buffer2 = NULL, **location = NULL;
-        char *token = NULL, *hint = "no";
-        int character = 0, interactMe = 1;
-        int pid = 0, status;
-	char **env = 0;
+	size_t bufsize = 0, h = 0, r;
+	char *buffer = NULL, *buffer2 = NULL, **location = NULL;
+	char *token = NULL, *hint = "no";
+	int character = 0, interactMe = 1, pid = 0, status;
 
-        while (interactMe)
-        {
-                interactMe = isatty(0); /* check for interactive mode */
-                if (interactMe)
-                        write(1, "$ ", 2);
-                /* printf("#cisfun$ "); */
-                character = getline(&buffer, &bufsize, stdin);
-                if (character == -1)
-                {
-                        putchar(10);
-                        break;
-                }
-                /* dup cmd line + save only what was first written */
-                buffer2 = strdup(buffer);
-                token = strtok(buffer2, "\n");
+	while (interactMe)
+	{
+		interactMe = isatty(0); /* check for interactive mode */
+		if (interactMe)
+			write(1, "$ ", 2);
+		character = getline(&buffer, &bufsize, stdin);
+		if (character == -1)
+		{
+			putchar(10);
+			break;
+		}
+		/* dup cmd line + save only what was first written */
+		buffer2 = strdup(buffer);
+		token = strtok(buffer2, "\n");
 		if (strcmp(token, "exit") == 0)
 		{
 			free(buffer2);
@@ -38,33 +35,33 @@ int main(__attribute__((unused))int argc ,__attribute__((unused))char **argv)
 			{
 				printf("%s\n", environ[r]);
 			}
-			free(token);
+			free(buffer2);
 			continue;
 		}
-                /* creates child proc for execve */
-                location = executeMe(token);
+		/* creates child proc for execve */
+		location = executeMe(token);
 		if (!location[0])
 		{
 			free(location[0]);
 			free(buffer);
 			return (0);
 		}
-                if (strcmp(location[0], hint) == 0) /* hardcode */
-                        continue;
-                pid = fork();
-                if (pid == 0)
-                {
-                        /* free(buffer), free(buffer2), free(location); */
-                        if (execve(location[0], location, env) == -1)
-                        {
+		if (strcmp(location[0], hint) == 0) /* hardcode */
+			continue;
+		pid = fork();
+		if (pid == 0)
+		{
+			/* free(buffer), free(buffer2), free(location); */
+			if (execve(location[0], location, environ) == -1)
+			{
 				perror(" "); /* logrado aca ejecuta y se termina */
 				return (0);
 			}
-                }
-                else
+		}
+		else
 			wait(&status);
-                free_array(location), free(buffer2), free(token);;
-        }
+		free_array(location), free(buffer2), free(token);
+	}
 	free(buffer);
-        return (0);
+	return (0);
 }
