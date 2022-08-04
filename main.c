c5f081cf3d6d72a2914511fd8a9cbf5168559410
Damian Caetano <5100@holbertonschool.com>
2022-08-04 07:26:37 -0700

    fix exit and env

--- main.c
+++ main.c
@@ -4,11 +4,12 @@
  */
 int main(__attribute__((unused))int argc ,__attribute__((unused))char **argv)
 {
-        size_t bufsize = 0;
+        size_t bufsize = 0, h = 0, r;
         char *buffer = NULL, *buffer2 = NULL, **location = NULL;
         char *token = NULL, *hint = "no";
         int character = 0, interactMe = 1;
         int pid = 0, status;
+	char **env = 0;
 
         while (interactMe)
         {
@@ -24,18 +25,35 @@ int main(__attribute__((unused))int argc ,__attribute__((unused))char **argv)
                 }
                 /* dup cmd line + save only what was first written */
                 buffer2 = strdup(buffer);
-                token = strtok(buffer2, " \n");
+                token = strtok(buffer2, "\n");
+		if (strcmp(token, "exit") == 0)
+		{
+			free(buffer);
+			return(h);
+		}
+		if (strcmp(token, "env") == 0)
+		{
+			for (r = 0; environ[r]; r++)
+			{
+				printf("%s\n", environ[r]);
+			}
+			continue;
+		}
                 /* creates child proc for execve */
                 location = executeMe(token);
+		if (!location[0])
+		{
+			free(location[0]);
+			free(buffer);
+			return (0);
+		}
                 if (strcmp(location[0], hint) == 0) /* hardcode */
                         continue;
                 pid = fork();
-                if (pid == -1)
-                        return (-1);
                 if (pid == 0)
                 {
                         /* free(buffer), free(buffer2), free(location); */
-                        if (execve(location[0], location, environ) == -1)
+                        if (execve(location[0], location, env) == -1)
                         {
 				perror(" "); /* logrado aca ejecuta y se termina */
 				return (0);
